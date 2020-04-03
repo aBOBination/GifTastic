7b25f009fefbd3360adc3ca1136c45f5ea96142d
Robert Marks <robert.marks54@gmail.com>
2020-04-02 21:36:52 -0600

    sets

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,42 @@
 // global vars
-var newSet = new Set(["bear", "cat", "cat"]);
-var gifArr = Array.from(newSet)
-
+var gifSet = new Set([]);
+var newGif = [];
 // function to do api calls
 
 // function to display gifs
+var gifTastic = {
+    addButton : function (gif) {
+        if(gif) {
+            var btn = $("<button>").text(gif)
+            $("#gif-buttons").append(btn)
+            gifSet.add(gif)
+            console.log(gifSet)
+        }
+    },
 
-// function to add buttons
-var addButton = function (gif) {
-    if(gif) {
-        var btn = $("<button>").text(gif)
-        $("#gif-buttons").append(btn)
-    }
+
 }
+// function to add buttons
+// var addButton = function (gif) {
+//     if(gif) {
+//         var btn = $("<button>").text(gif)
+//         $("#gif-buttons").append(btn)
+//     }
+// }
 
 /////////////////////////////////////
 window.onload = function() {
-    for(var i = 0; i < gifArr.length; i++) {
-        addButton(gifArr[i])
-    }
+    this.gifSet.forEach(this.gifTastic.addButton)
+    // for(var i = 0; i < gifSet.length; i++) {
+    //     gifTastic.addButton(gifSet[i])
+
 };
 
 // on click when adding new button
 $(document).on( "click", "#add-gif", function(event){
     event.preventDefault();
-    addButton($("#gif").val())
+    gifTastic.addButton($("#gif").val().toLowerCase())
     $("#gif").val("")
-
 } );
 // on click to display gifs
 $(document).on( "click", ".add-btn", function(){
