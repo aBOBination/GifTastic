35fa6b997e83592aad22ffcbef25f93c1ed0d7f8
Robert Marks <robert.marks54@gmail.com>
2020-04-04 01:07:49 -0600

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # GifTastic
+
+This page uses Javascript and JQuery for accessing the Giphy API to display 10 random gifs for the topic selected.  The Javascript is organized as an Object with 2 methods under it.
+
+The two methods are:
+
+`addButton`
+> This method add the buttons to the page on load and when a user submits a new topic for the page.
+
+`displayGifs`
+> This method displays all 10 gifs when a user clicks on the button.  This is where it reaches out to the Giphy API and does a serach for 10 giphys related to the topic the button displays.
+
+---
+
+`Files Structure`
+
+```
+gifTastic
+│
+├──── assets
+│    │
+│    ├──── css
+│    │      └──── style.css
+│    │
+│    └──── javascript
+│           └──── app.js
+│
+|
+├──── index.html
+│
+└──── README.md
+```
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,60 @@
-// global vars
 var topics = ["bear", "camel", "chicken", "duck", "giraffe", "hippo",
               "grumpy cat", "kangaroo", "koala", "otter", "panda",
               "sheep", "tiger", "turtle", "unicorn"];
 
-// function to do api calls
 
-// function to display gifs
 var gifTastic = {
-    //method to add buttons
     addButton : function (gif) {
-        var btn = $("<button>").text(gif)
+        var btn = $("<button>").addClass("btn btn-display m-1").attr("topic", gif).text(gif)
         $("#gif-buttons").append(btn)
     },
-
-    getData : function(gif) {
+    displayGifs : function(gif) {
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wv4iP1f8xEfchiArmZMS4oeh0FtMM5uH&q=" + gif + "&limit=10&offset=0&rating=G&lang=en";
         $.ajax({
-            url: queryURL,
-            method: "GET",
-            success: function(result){
-                console.log(result.data)
+                url: queryURL,
+                method: "GET"
+        }).then(function(response) {
+            $(".display-gifs").empty()
+            var data = response.data;
+            for(var i = 0; i < data.length; i++) {
+                var imgTitle = data[i].title
+                var ImgRating = data[i].rating;
+                var imgSrcStill = data[i].images.fixed_width_still.url;
+                var imgSrcAnimated = data[i].images.fixed_width.url;
+                var gifDiv = $("<div>").addClass("card")
+                var imgdiv = $("<img>").addClass("card-img-top").attr({"src" : imgSrcStill, "data-still" :  imgSrcStill, "data-animate" :  imgSrcAnimated, "data-state" : "still"})
+                var imgBody = $("<div>").addClass("card-body")
+                var titleDiv = $("<p>").addClass("card-title").html("<strong>" + imgTitle + "</strong>")
+                var ratingDiv = $("<p>").html("Rated: " + "<strong>" + ImgRating + "</strong>")
+                gifDiv.append(imgdiv, imgBody)
+                imgBody.append(titleDiv, ratingDiv)
+                $(".display-gifs").append(gifDiv)
             }
+            $(".card-img-top").on("click", function() {
+                var dataState =  $(this).attr("data-state")
+                if (dataState === "still") {
+                    var dataAnimate = $(this).attr("data-animate");
+                    $(this).attr("src", dataAnimate);
+                    $(this).attr("data-state", "animate");
+                } else if (dataState === "animate") {
+                    var dataStill = $(this).attr("data-still");
+                    $(this).attr("src", dataStill);
+                    $(this).attr("data-state", "still");
+                }
+            })
         });
     }
 };
-gifTastic.getData("bear")
-/////////////////////////////////////
+
 $( window ).on( "load", function() {
     topics.forEach(topic => {gifTastic.addButton(topic)});
 });
 
-// on click when adding new button
 $(document).on( "click", "#add-btn", function(event){
     var newGif = $("#gif").val()
     if(newGif && topics.includes(newGif) === false) {
+        topics.push(newGif)
         event.preventDefault();
-        console.log(newGif)
         gifTastic.addButton(newGif)
         $("#gif").val("")
     } else {
@@ -44,11 +63,6 @@ $(document).on( "click", "#add-btn", function(event){
     }
 });
 
-// // on click to display gifs
-// $(document).on( "click", ".add-gifs", function(){
-//     // do things
-// } );
-// // on click to remove button
-// $(document).on( "click", ".del-btn", function(){
-//     // do things
-// } );
+$(document).on( "click", ".btn-display", function(){
+    gifTastic.displayGifs($(this).attr("topic"))
+} );
